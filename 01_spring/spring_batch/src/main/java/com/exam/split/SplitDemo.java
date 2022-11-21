55cc01e926a8f6537ccc458efddd834ac657ad8d
chenlt <da.mu@163.com>
2022-11-21 17:27:02 +0800

    Add batch demo

---
+++ 01_spring/spring_batch/src/main/java/com/exam/split/SplitDemo.java
@@ -0,0 +1,21 @@
+package com.exam.split;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class SplitDemo {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+
+
+}

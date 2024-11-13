64cd5144ceb4ad34aaf03ef784813dadfc507c35
chenlt-qtl <da.mu@163.com>
2024-11-13 09:57:10 +0800

    代码

---
+++ 13_java/D01_Common/src/test/java/com/betta/D01CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D01CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
